e7fd1a09566a3da9512ae1ddbb1dda53b56a8818
Vandelson Cleitoso <julim.generic@gmail.com>
2023-06-08 12:23:05 -0300

    Shit dont work anymore, anyway
    here's the requisition and maybe a project of a form.

---
+++ cagekeeper/src/main/java/br/com/cagekeeper/cagekeeper/dto/RequisicaoNovoUsuario.java
@@ -0,0 +1,50 @@
+package br.com.cagekeeper.cagekeeper.dto;
+import br.com.cagekeeper.cagekeeper.Usuario;
+import br.com.cagekeeper.cagekeeper.dao.NewRepository;
+
+public class RequisicaoNovoUsuario {
+    private Long usuarioId;
+    private String usuarioNome;
+    private String usuarioPassword;
+    private String usuarioDados;
+
+    public Long getUsuarioId() {
+        return this.usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getUsuarioNome() {
+        return this.usuarioNome;
+    }
+
+    public void setUsuarioNome(String usuarioNome) {
+        this.usuarioNome = usuarioNome;
+    }
+
+    public String getUsuarioPassword() {
+        return this.usuarioPassword;
+    }
+
+    public void setUsuarioPassword(String usuarioPassword) {
+        this.usuarioPassword = usuarioPassword;
+    }
+
+    public String getUsuarioDados() {
+        return this.usuarioDados;
+    }
+
+    public void setUsuarioDados(String usuarioDados) {
+        this.usuarioDados = usuarioDados;
+    }
+    public Usuario toUsuario(NewRepository usuario){
+        Usuario usuario = new Usuario();
+        usuario.setNome(usuarioNome);
+        usuario.setPassword(usuarioPassword);
+        usuario.setDados(usuarioDados);
+        return usuario;
+    }
+
+}
