0249f58db06b285c7f1af94d1267ffef23bd5bcf
Vandelson Cleitoso <julim.generic@gmail.com>
2023-06-27 20:00:19 -0300

    Coloquei o formulario no usuario controller
    Criação de conta ainda não funciona

--- cagekeeper/src/main/java/br/com/cagekeeper/cagekeeper/controller/UsuarioController.java
+++ cagekeeper/src/main/java/br/com/cagekeeper/cagekeeper/controller/UsuarioController.java
@@ -1,18 +1,40 @@
 package br.com.cagekeeper.cagekeeper.controller;
 
+
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.cagekeeper.cagekeeper.Usuario;
 import br.com.cagekeeper.cagekeeper.dao.NewRepository;
+import br.com.cagekeeper.cagekeeper.dao.UsuarioRepository;
+import br.com.cagekeeper.cagekeeper.dto.RequisicaoNovoUsuario;
 @Controller
 @RequestMapping("usuario")
 public class UsuarioController {
 
 	@Autowired
-	private NewRepository usuarioRepository;
-	
+	private NewRepository newRepository;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	@GetMapping("formulario")
+	public String formulario(RequisicaoNovoUsuario requisicaoNovoUsuario) {
+		return "/formulario";
+	}
+	@PostMapping("novo")
+	public String novo(@jakarta.validation.Valid RequisicaoNovoUsuario requisicaoNovoUsuario, BindingResult result) {
+		if(result.hasErrors()) {
+			return "/formulario";
+		} else {
+			Usuario f = requisicaoNovoUsuario.toUsuario(newRepository);
+			this.newRepository.save(f);
+			return "redirect:/home";
+		}
+}
 }
